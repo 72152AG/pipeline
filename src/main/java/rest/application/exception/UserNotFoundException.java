c7ced85e2fc928285eec09c849e2121d1d1282b9
BM <72152@gwsh.pl>
2024-02-17 17:17:57 +0100

    Pipeline

--- src/main/java/rest/application/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package rest.application.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("Could not found user with id:" + id);
-    }
-}
